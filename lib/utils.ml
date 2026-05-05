9cb2c7ae0844e0d1eb244f41bfacc8107ca3b649
Rouzbeh <rouzbeh.m.yazdi@gmail.com>
2026-05-05 07:43:40 -0400

    add ability to distinguish os

--- lib/utils.ml
+++ lib/utils.ml
@@ -14,9 +14,51 @@ let print_time_banner (banner : string) =
   print_endline formatted_time
 ;;
 
-let get_mode_as_int () =
+let prompt_for_integer () =
   print_endline "Enter the target mode: \n 1. Work 2. Idle \n";
   read_int ()
 ;;
 
 let print_in_situ (message : string) = Printf.printf "\r\027[K%s%!" message
+
+type operating_system =
+  | MacOS
+  | Linux
+  | FreeBSD
+  | OpenBSD
+  | NetBSD
+  | DragonFly
+  | Cygwin
+  | Win32
+  | Unix
+  | Other of string
+
+let get_os_name () =
+  match Sys.os_type with
+  | "Unix" ->
+    let ic = Unix.open_process_in "uname" in
+    let name = input_line ic in
+    ignore (Unix.close_process_in ic);
+    (match String.trim name with
+     | "Darwin" -> MacOS
+     | "Linux" -> Linux
+     | "FreeBSD" -> FreeBSD
+     | "OpenBSD" -> OpenBSD
+     | "NetBSD" -> NetBSD
+     | "DragonFly" -> DragonFly
+     | _ -> Unix)
+  | "Win32" -> Win32
+  | "Cygwin" -> Cygwin
+  | s -> Other s
+;;
+
+let alert () =
+  let name = get_os_name () in
+  match name with
+  | MacOS ->
+    ignore
+      (Sys.command
+         "osascript -e 'tell application \"System Events\" to display dialog \"Timer is \
+          Finished!\" with title \"RTimer\" buttons {\"OK\"} default button \"OK\"'")
+  | _ -> ()
+;;
